7c0ec2e6f16a052279a669d9a5725b610e35fc93
Aashray Jain <aashrayjainkatni@gmail.com>
2021-02-27 11:52:24 +0530

    nav-bar and data-table html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { DataTableComponent } from './data-table/data-table.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    MenuBarComponent,
-    DataTableComponent
+    DataTableComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
