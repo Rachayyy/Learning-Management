25237cd43d31d012a98aa77eb58042de7b1897ce
Aashray Jain <aashrayjainkatni@gmail.com>
2021-02-28 14:58:12 +0530

    user course assign form validation done

--- src/app/components/assign-course-to-user/assign-course-to-user.component.ts
+++ src/app/components/assign-course-to-user/assign-course-to-user.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+class User {
+  constructor(
+    public userId: number,
+    public userName: string,
+    public createDate: Date
+  ) { }
+}
+
 @Component({
   selector: 'app-assign-course-to-user',
   templateUrl: './assign-course-to-user.component.html',
@@ -9,8 +17,24 @@ export class AssignCourseToUserComponent implements OnInit {
 
   constructor() { }
 
+  public userId: number = 1;
+  public userName: string = "Aashray";
+  public createDate: Date = new Date();
+  public courseName: string = "ABC";
+  public courseCode: number;
+  public courses: number[] = [
+    101, 102, 103, 104, 405, 106, 107
+  ];
+
+  user: User = new User(this.userId, this.userName, this.createDate);
+  
+
   ngOnInit(): void {
     console.log("Hello")
+  }
+
+  assignCourse() {
+    console.log(this.user.userId + " " + this.user.userName + " " + this.user.createDate + " " + this.courseCode + " " + this.courseName)
   }
 
 }
