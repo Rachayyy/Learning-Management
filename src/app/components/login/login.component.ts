a56754f8b73ce4a0b4b3855ac30ced5e8e0359de
Rink08 <rinkesh.kore35@gmail.com>
2021-03-03 10:41:16 +0530

    admin n user in login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,16 +14,29 @@ export class LoginComponent implements OnInit {
 
    public userName: string;
    public pass: string;
+   public value:string;
   
 
   ngOnInit(): void {
   }
    
   LoginUser(){
+    if (this.value == "admin"){
      if(this.userName == "Admin" && this.pass == "1234"){
         alert('login successful!!');
         this.router.navigate(['/dashboard']);
      }
-     
+     else{
+      alert('username or password is wrong');
+     }
+    }
+
+    if(this.value == "user"){
+      
+    }
+  }
+
+  onItemChange(value1){
+      this.value = value1;
   }
 }
