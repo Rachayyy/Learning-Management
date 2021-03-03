eb1e23d047c434acb8c86bba8cd5c63259f0d6fe
Aashray Jain <aashrayjainkatni@gmail.com>
2021-03-03 12:51:48 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,8 @@ import { FeaturesComponent } from './components/features/features.component';
 import { CompanyComponent } from './components/company/company.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-<<<<<<< HEAD
-import { UserdashboardComponent } from './components/userdashboard/userdashboard.component';
-=======
 import { UsersComponent } from './components/users/users.component';
 import { CreditLogsComponent } from './components/credit-logs/credit-logs.component';
->>>>>>> d6e231f5b0f0e8443a00fa0ab24977a86f272168
 
 const routes: Routes = [
   { path: 'assign/:user', component: AssignCourseToUserComponent },
@@ -25,12 +21,8 @@ const routes: Routes = [
   { path: 'company', component: CompanyComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'dashboard', component: DashboardComponent },
-<<<<<<< HEAD
-  { path: 'userdashboard', component: UserdashboardComponent },
-=======
   { path: 'users', component: UsersComponent },
   { path: 'credit', component: CreditLogsComponent },
->>>>>>> d6e231f5b0f0e8443a00fa0ab24977a86f272168
   { path: '', redirectTo: '/login', pathMatch: 'full'  },
   { path: '**', component: PageNotFoundComponent }
 ];
