00f546e130747c0449038aea196244a25ca9389d
Rink08 <rinkesh.kore35@gmail.com>
2021-03-04 21:59:18 +0530

    user info

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,23 +11,16 @@ export class UsersComponent implements OnInit {
   constructor(private httpService: HttpserviceService) { }
    user = [];
   val = [];
- 
-  ngOnInit(): void {
+
+   ngOnInit(): void {
     this.getAllUsers();
   }
  getAllUsers(){
      this.httpService.getUser().subscribe((res) => {
        this.user = Object.values(res);
       this.val = Object.values(this.user);
-      // for (let i of this.val.values()){
-      //   for (var j =0;j< i.length;j++)
-      //           console.log(i[j].userId);
-      //  }
-      //  this.arr = Object.keys(this.user).map(key => ({type: key, value: this.user[key]}));
-      //  console.log(this.arr);
        if(res)
        console.log("ok");
-      console.log(this.val.values());
      }, err => console.log("faild"));
  }
 }
