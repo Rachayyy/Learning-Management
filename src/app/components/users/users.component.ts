c63b9c980666c15c047c42c316f3f2a7481b15c6
Rink08 <rinkesh.kore35@gmail.com>
2021-03-04 21:41:31 +0530

    user

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpserviceService } from 'src/app/services/httpservice.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private httpService: HttpserviceService) { }
+   user = [];
+  val = [];
+ 
   ngOnInit(): void {
+    this.getAllUsers();
   }
-
+ getAllUsers(){
+     this.httpService.getUser().subscribe((res) => {
+       this.user = Object.values(res);
+      this.val = Object.values(this.user);
+      // for (let i of this.val.values()){
+      //   for (var j =0;j< i.length;j++)
+      //           console.log(i[j].userId);
+      //  }
+      //  this.arr = Object.keys(this.user).map(key => ({type: key, value: this.user[key]}));
+      //  console.log(this.arr);
+       if(res)
+       console.log("ok");
+      console.log(this.val.values());
+     }, err => console.log("faild"));
+ }
 }
