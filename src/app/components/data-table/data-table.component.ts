eb6248acde442ae8999d6db37ec89c2e70067983
Rink08 <rinkesh.kore35@gmail.com>
2021-03-04 22:14:53 +0530

    course table

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpserviceService } from 'src/app/services/httpservice.service';
 
 @Component({
   selector: 'app-data-table',
@@ -7,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpserviceService) { }
+  course = [];
+  val = [];
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+    this.getAllCourse();
   }
+ getAllCourse(){
+     this.httpService.getCourse().subscribe((res) => {
+       this.course = Object.values(res);
+      this.val = Object.values(this.course);
+       if(res)
+       console.log("ok");
+     }, err => console.log("faild"));
+ }
 
 
 }
